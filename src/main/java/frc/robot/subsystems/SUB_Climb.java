8d9e45066d755f5c30a94428908d14ac85326706
Justin Nguyen <justinnguyen4675@gmail.com>
2022-01-11 19:48:46 -0500

    Intial push

---
+++ src/main/java/frc/robot/subsystems/SUB_Climb.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+/** Add your docs here. */
+public class SUB_Climb {
+    private Solenoid m_Solenoid = new Solenoid(2);
+    private Solenoid m_WinchRatchet = new Solenoid(0);
+
+    public SUB_Climb(){
+
+    }
+
+    public void setClimberUp(){
+        m_Solenoid.set(true);
+    }
+
+    public void setClimberDown(){
+        m_Solenoid.set(false);
+    }
+
+    public void setReleaseWinchRatchet(){
+        m_WinchRatchet.set(true);
+    }
+
+    public void setEngageWinchRatchet(){
+        m_WinchRatchet.set(false);
+    }
+}
